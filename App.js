a5eac3511137e6e5b90c48e993f10e513489109d
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-11-16 17:15:27 -0600

    update

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const App = () => {
           component={HomeScreen}
           options={{
             title: "Profile App",
-            headerTintColor: "#fff",
+            headerTintColor: "#000",
             headerStyle: {
               backgroundColor: "purple",
             },
